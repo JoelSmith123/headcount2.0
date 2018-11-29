515b14da82331af7d94b8bf6aba311d2035222c9
Joel Smith <joelwsmith123@gmail.com>
2018-11-28 17:20:02 -0700

    finish iteration 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import kinderData from './data/kindergartners_in_full_day_program.js';
+import './App.css'
+import CardContainer from './CardContainer'
+import DistrictRepository from './helper'
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      repository: new DistrictRepository(kinderData),
+      searchVal: ''
+    }
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({
+      searchVal: event.target.value
+    })
+  }
+
   render() {
+    const { findAllMatches, findByName } = this.state.repository
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>
+        <h1 className='title'>Welcome To Headcount 2.0</h1>
+        <input className='search' type='search' value={this.state.searchVal} onChange={this.handleSearchChange} placeholder=' Search...' />
+        <CardContainer findAllMatches={findAllMatches} findByName={findByName} searchVal={this.state.searchVal} />
+      </div>
     );
   }
 }
